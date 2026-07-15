d82e166fd7efe493aeb32f762f9b692e5feac7e7
Alee24 <mettoalex@gmail.com>
2026-07-16 01:32:11 +0300

    fix(main_layout): import dart:io for InternetAddress reference

--- frontend/lib/core/main_layout.dart
+++ frontend/lib/core/main_layout.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'dart:async';
+import 'dart:io';
 import '../features/chat/socket_service.dart';
 
 class MainLayout extends ConsumerStatefulWidget {
